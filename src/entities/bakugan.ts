b9b82e249847ea664d7e2975a99be80c32ced2dd
Ana Luiza Landim Irschlinger <20221PF.CC0014@passofundo.ifsul.local>
2026-05-12 09:05:06 -0300

    feat: setup inicial

---
+++ src/entities/bakugan.ts
@@ -0,0 +1,15 @@
+export class Bakugan {
+  constructor(
+    public id: number,
+    public name: string,
+    public attribute: string,
+    public power: number
+  ) {
+        this.id = id;
+        this.name = name;
+        this.attribute = attribute;
+        this.power = power;
+  }
+
+ 
+}
